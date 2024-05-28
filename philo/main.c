c9a5e076ddabcf19f43227ef2ad0481f16dda41e
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-28 20:38:50 -0300

    Add main.c file with initial steps of the project

---
+++ philo/main.c
@@ -0,0 +1,9 @@
+#include "philo.h"
+
+int	main(int argc, char **argv)
+{
+	// check if the correct nbr of arguments were provided -> count with the nbr of meals
+	// parse the args
+	// init philos and forks
+	return (0);
+}
