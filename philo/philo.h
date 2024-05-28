c897b1e0ed3a728b6d6eb8e5fee57fd9dbb4f7ee
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-28 20:40:34 -0300

    Add initial header file with all allowed external libraries

---
+++ philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 20:19:34 by tsoares-          #+#    #+#             */
+/*   Updated: 2024/05/28 20:24:01 by tsoares-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#endif
