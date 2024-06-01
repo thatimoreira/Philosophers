44ce31bf23e92e435a4e8e794e432dd4b042002a
thatimoreira <thatimoreira1@gmail.com>
2024-06-01 19:16:21 -0300

    refactor: Replace printf calls with write function to print error messages to stderr (file descriptor 2) for better error handling and consistency.

--- philo/srcs/validate_args.c
+++ philo/srcs/validate_args.c
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 00:21:35 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/06/01 03:40:14 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/06/01 18:58:40 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,23 +39,23 @@ int	validate_args(int args_count, char **args_value)
 {
 	if (args_count != 5 && args_count != 6)
 	{
-		printf("Usage: %s number_of_philosophers time_to_die"
-			"time_to_eat time_to_sleep"
-			"[number_of_times_each_philosopher_must_eat]\n",
-			args_value[0]);
+		write(2, "Usage: ./philo number_of_philosophers time_to_die"
+			" time_to_eat time_to_sleep"
+			" [number_of_times_each_philosopher_must_eat]\n",
+			121);
 		return (0);
 	}
 	while (*(args_value + 1))
 	{
 		if (!is_greater_than_zero(str_to_long(*(args_value + 1))))
 		{
-			printf("Error: All arguments must be greater than"
-				" zero.\n");
+			write(2, "Error: All arguments must be greater than"
+				" zero.\n", 48);
 			return (0);
 		}
 		if (!is_int(*(args_value + 1)))
 		{
-			printf("Error: All arguments must be numbers.\n");
+			write(2, "Error: All arguments must be numbers.\n", 38);
 			return (0);
 		}
 		args_value++;
