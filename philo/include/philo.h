32d70ff263a97070c534451ef358ee1d94364284
thatimoreira <thatimoreira.dev@gmail.com>
2024-06-02 02:16:49 -0300

    Add philosopher and shared data structs
    Introduced two new structs: 't_philo'and 't_shared_data'. The 't_philo' struct represents the state and attributes of each philosopher, including their ID, fork indices, last meal timestamp, and meals eaten. The 't_shared_data' struct holds common data for all philosopher, such as the number of philosophers, timing information, and thread synchronization mechanisms.

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 20:19:34 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/06/01 01:03:19 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/06/02 01:56:04 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,35 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <pthread.h>
+
+#define MAX_PHILOSOPHERS 5
+
+// struct to store each philosopher state and attributes
+typedef struct s_philo
+{
+	int		philo_id;
+	int		fork_left; // index of the left fork
+	int		fork_right; // index of the right fork
+	long long	last_meal_time; // timestamp for the last meal
+	int		meals_eaten;
+	pthread_t	philo_thread; // thread handle for the philosopher
+}			t_philo;
+
+// struct that holds shared data that is common to all philosophers, such as
+// the nbr of philosophers, timing info, and synchronization primitives
+typedef struct s_shared_data
+{
+	int		number_of_philosophers;
+	long long	time_to_die;
+	long long	time_to_eat;
+	long long	time_to_sleep;
+	int		number_of_times_each_philosopher_must_eat;
+	int		stop_simulation;
+	long long	start_time;
+	pthread_mutex_t	*fork;
+	pthread_mutex_t	print_lock; // mutex used for synchronization in multithreading
+	t_philo		*philo;
+}			t_shared_data;
 
 long	str_to_long(const char *str);
 int	validate_args(int args_count, char **args_value);
