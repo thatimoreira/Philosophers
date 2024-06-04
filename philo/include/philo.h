215f40d602b146b09ee8d43e8418c6528093a80b
thatimoreira <thatimoreira.dev@gmail.com>
2024-06-04 04:27:53 -0300

    Enhance 'philo.h' with additional philosopher attributes, update it to include left and right hand mutexes, extend access to shared data, and declared new essential functions to execute the philosophers dinner simulation.

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 20:19:34 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/06/02 01:56:04 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/06/02 04:19:43 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,31 +25,36 @@
 // struct to store each philosopher state and attributes
 typedef struct s_philo
 {
-	int		philo_id;
-	int		fork_left; // index of the left fork
-	int		fork_right; // index of the right fork
-	long long	last_meal_time; // timestamp for the last meal
-	int		meals_eaten;
-	pthread_t	philo_thread; // thread handle for the philosopher
-}			t_philo;
+	int						philo_id;
+	pthread_mutex_t			*left_hand;
+	pthread_mutex_t			*right_hand;
+	long long				last_meal_time; // timestamp for the last meal
+	int						meals_eaten;
+	pthread_t				philo_thread; // thread handle for the philosopher
+	struct s_shared_data	*shared_data; // give each philo access to shared data
+}							t_philo;
 
 // struct that holds shared data that is common to all philosophers, such as
 // the nbr of philosophers, timing info, and synchronization primitives
 typedef struct s_shared_data
 {
-	int		number_of_philosophers;
-	long long	time_to_die;
-	long long	time_to_eat;
-	long long	time_to_sleep;
-	int		number_of_times_each_philosopher_must_eat;
-	int		stop_simulation;
-	long long	start_time;
-	pthread_mutex_t	*fork;
+	int				nbr_philos;
+	long long		time_to_die;
+	long long		time_to_eat;
+	long long		time_to_sleep;
+	int				times_philo_must_eat;
+	int				stop_simulation;
+	long long		start_time;
+	pthread_mutex_t	*forks;
 	pthread_mutex_t	print_lock; // mutex used for synchronization in multithreading
-	t_philo		*philo;
-}			t_shared_data;
+	t_philo			*philos; // pointer to array of philosophers
+}					t_shared_data;
 
 long	str_to_long(const char *str);
-int	validate_args(int args_count, char **args_value);
+int		validate_args(int args_count, char **args_value);
+int		init_simulation(char **values);
+void	*philosopher_routine(void *args);
+void	init_mutexes(t_shared_data *shared_data);
+void	init_philosopher_data(t_shared_data *shared_data, int i);
 
 #endif
