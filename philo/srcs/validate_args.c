a572c2a2e1935e079d687d4a398154bc12592051
thatimoreira <thatimoreira.dev@gmail.com>
2024-06-04 04:42:01 -0300

    Replace printf calls with write function to print error messages to stderr (file descriptor 2) for better error handling and consistency.

--- philo/srcs/validate_args.c
+++ philo/srcs/validate_args.c
@@ -12,53 +12,53 @@
 
 #include "../include/philo.h"
 
-int	is_digit(int c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
-}
-
-int	is_int(const char *s)
-{
-	while (*s)
+	int	is_digit(int c)
 	{
-		if (!is_digit(*s))
-			return (0);
-		s++;
-	}
-	return (1);
-}
-
-int	is_greater_than_zero(long value)
-{
-	return (value > 0);
-}
-
-int	validate_args(int args_count, char **args_value)
-{
-	if (args_count != 5 && args_count != 6)
-	{
-		write(2, "Usage: ./philo number_of_philosophers time_to_die"
-			" time_to_eat time_to_sleep"
-			" [number_of_times_each_philosopher_must_eat]\n",
-			121);
+		if (c >= '0' && c <= '9')
+			return (1);
 		return (0);
 	}
-	while (*(args_value + 1))
+
+	int	is_int(const char *s)
 	{
-		if (!is_greater_than_zero(str_to_long(*(args_value + 1))))
+		while (*s)
 		{
-			write(2, "Error: All arguments must be greater than"
-				" zero.\n", 48);
-			return (0);
+			if (!is_digit(*s))
+				return (0);
+			s++;
 		}
-		if (!is_int(*(args_value + 1)))
-		{
-			write(2, "Error: All arguments must be numbers.\n", 38);
-			return (0);
-		}
-		args_value++;
+		return (1);
 	}
-	return (1);
-}
+
+	int	is_greater_than_zero(long value)
+	{
+		return (value > 0);
+	}
+
+	int	validate_args(int args_count, char **args_value)
+	{
+		if (args_count != 5 && args_count != 6)
+		{
+			write(2, "Usage: ./philo number_of_philosophers time_to_die"
+				" time_to_eat time_to_sleep"
+				" [number_of_times_each_philosopher_must_eat]\n",
+				121);
+			return (0);
+		}
+		while (*(args_value + 1))
+		{
+			if (!is_greater_than_zero(str_to_long(*(args_value + 1))))
+			{
+				write(2, "Error: All arguments must be greater than"
+					" zero.\n", 48);
+				return (0);
+			}
+			if (!is_int(*(args_value + 1)))
+			{
+				write(2, "Error: All arguments must be numbers.\n", 38);
+				return (0);
+			}
+			args_value++;
+		}
+		return (1);
+	}
