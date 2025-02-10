0a01083efd8cf1e4d138f77e3d88a1a95bd2d524
thatimoreira <thatimoreira.dev@gmail.com>
2025-02-10 18:59:29 -0300

    Implement header file

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 15:23:23 by tsoares-          #+#    #+#             */
-/*   Updated: 2025/02/10 15:47:08 by tsoares-         ###   ########.fr       */
+/*   Updated: 2025/02/10 16:40:22 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 //--- INIT --------------------------------------------------------------------
 
 //--- INPUT VALIDATION --------------------------------------------------------
+bool	is_valid_input(int n_args);
+bool	print_return(int fd, char *message, bool return_value, int n_chars);
 
 //--- PARSE ARGS --------------------------------------------------------------
 
