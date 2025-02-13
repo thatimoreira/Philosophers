373b77000782e40d3cb2f35593274672622f6458
thatimoreira <thatimoreira1@gmail.com>
2025-02-13 04:19:08 -0300

    Implement macros to set min and max number of philosophers

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 15:23:23 by tsoares-          #+#    #+#             */
-/*   Updated: 2025/02/10 16:40:22 by tsoares-         ###   ########.fr       */
+/*   Updated: 2025/02/12 18:47:24 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <limits.h>
+
+# define MIN_PHILO  1
+# define MAX_PHILO 200
+# define INT_MIN INT_MAX
 
 //--- INIT --------------------------------------------------------------------
 
 //--- INPUT VALIDATION --------------------------------------------------------
 bool	is_valid_input(int n_args);
-bool	print_return(int fd, char *message, bool return_value, int n_chars);
+bool	msg_return(int fd, char *message, bool return_value, int n_chars);
 
 //--- PARSE ARGS --------------------------------------------------------------
 
-#endif
+# endif
