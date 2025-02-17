2cc0e4b500bc08b646ad5a0c5155f41340d6ae3c
thatimoreira <thatimoreira.dev@gmail.com>
2025-02-16 22:56:00 -0300

    Fix: moves validate_input to process_args and fix input validation logic

--- srcs/process_args/validate_input.c
+++ srcs/process_args/validate_input.c
@@ -12,37 +12,21 @@
 
 #include "../../includes/philo.h"
 
-bool	ft_is_digit(int c)
-{
-	if (c >= '0' || c <= '9')
-		return (true);
-	return (false);
-}
-
 int	is_valid_input(int argc, char **argv, t_table *table)
 {
-	/*int	i;
-	long	num;
-
-	i = 0;
-	num = 0;*/
-	(void)argv;
-	(void)table;
-
-	if (argc == 5 || argc == 6)
-		//validate_args
-		write(STDOUT_FILENO, "Valid number of parameters\n", 27);
-	else
+	if (argc != 5 && argc != 6)
 	{
 		if (argc == 1)
-			print_error("Error: no parameters provided\n");
+			write(STDERR_FILENO, "Error: no parameters provided\n", 31);
 		else if (argc < 5)
-			print_error("Error: too few parameters\n");
+			write(STDERR_FILENO, "Error: too few parameters\n", 26);
 		else
-			print_error("Error: too many parameters\n");
+			write(STDERR_FILENO, "Error: too many parameters\n", 27);
 		printf("Usage: nbr_philos  time_to_die time_to_eat time_to_sleep ");
 		printf("[times_each_philo_must_eat]\n");
 		return (-1);
 	}
+	if (!parse_args(argc, argv, table))
+		return (-1);
 	return (1);
 }
